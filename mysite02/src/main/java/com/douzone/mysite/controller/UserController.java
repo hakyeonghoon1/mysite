2dfd6e5218579972ea93c1bc7de086c337423b36
hakyeonghoon1 <skyhoon8@gmail.com>
2021-10-12 14:05:51 +0900

    Action & ActionFactory

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -2,19 +2,16 @@ package com.douzone.mysite.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.mysite.mvc.MainActionFactory;
 import com.douzone.mysite.mvc.UserActionFactory;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.mvc.ActionFactory;
-import com.douzone.web.util.MvcUtil;
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,13 +19,22 @@ public class MainController extends HttpServlet {
 		
 		String actionName = request.getParameter("a");
 		
-		ActionFactory af = new MainActionFactory();
+		ActionFactory af = new UserActionFactory();
 		Action action = af.getAction(actionName);
 		action.execute(request, response);
+		
+//		if("joinform".equals(action)) {
+//			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
+//		} else if("join".equals(action)) {
+//			
+//		} else {
+//			MvcUtil.redirect("/mysite02", request, response);
+//		}
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	
 		doGet(request, response);
 	}
 

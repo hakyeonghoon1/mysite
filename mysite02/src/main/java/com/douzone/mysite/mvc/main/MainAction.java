c3a643a12cfd0a88fcbe2b77af446c08fe1b4675
hakyeonghoon1 <skyhoon8@gmail.com>
2021-10-12 17:23:19 +0900

    user service

--- mysite02/src/main/java/com/douzone/mysite/mvc/main/MainAction.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/main/MainAction.java
@@ -1,4 +1,4 @@
-package com.douzone.mysite.mvc;
+package com.douzone.mysite.mvc.main;
 
 import java.io.IOException;
 
@@ -9,11 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.util.MvcUtil;
 
-public class JoinFormAction implements Action {
+public class MainAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		MvcUtil.forward("user/joinform", request, response);
+		MvcUtil.forward("main/index", request, response);
 
 	}
 

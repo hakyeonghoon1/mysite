17e05f42b1e2fcf929a3f8cda9ced68ac4e240fc
hakyeonghoon1 <skyhoon8@gmail.com>
2021-11-10 19:47:13 +0900

    mysite06

--- mysite06/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite06/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -3,14 +3,11 @@ package com.douzone.mysite.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.douzone.mysite.service.AdminService;
-import com.douzone.mysite.vo.SiteVo;
 
 
 @Controller
